23a12f77aa4010768333852a4ba6673289081955
Geoffrey Hannigan <ghanni@upenn.edu>
2016-02-29 12:36:02 -0500

    Added SQL script to extract mapping file information. Please excise the hardcoding.

---
+++ bin/ExtractSequenceMap.sql
@@ -0,0 +1,10 @@
+/* Set the working database */;
+use SchlossHanniganSequencing;
+
+/* Get the adapter information */;
+SELECT * 
+FROM SampleTracking, IndexPrimer001, IndexPrimer002 
+WHERE SampleTracking.Index1=IndexPrimer001.Index1 
+      AND SampleTracking.Index2=IndexPrimer002.Index2 
+      AND SampleTracking.SchlossID="NexteraXT001" 
+INTO OUTFILE '/Users/Hannigan/git/Hannigan-2016-ColonCancerVirome/data/NexteraXT001Map.tsv';
