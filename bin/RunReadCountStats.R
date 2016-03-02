f7aea7ab95dd91ac363a00cfb483f92b733ba992
Geoffrey Hannigan <ghanni@upenn.edu>
2016-03-02 09:27:02 -0500

    Added R script for plotting output data.

---
+++ bin/RunReadCountStats.R
@@ -0,0 +1,45 @@
+# RunReadCountStats.R
+# Geoffrey Hannigan
+# Pat Schloss Lab
+# University of Michigan
+
+#################
+# Set Libraries #
+#################
+
+library("ggplot2")
+library("RColorBrewer")
+library("gridExtra")
+
+#################################
+# Parse Input from Command Line #
+#################################
+
+option_list = list(
+	make_option(c("-c", "--counts"), type="character", default=NULL, 
+			help="table with read count information", metavar="character"),
+	make_option(c("-o", "--output"), type="character", default=NULL, 
+			help="output file for count summary", metavar="character")
+); 
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+#################
+# Read In Files #
+#################
+
+COUNTS <- read.delim(file=opt$counts, sep="\t", header=F)
+
+############
+# Run Data #
+############
+
+Summary <- summary(COUNTS)
+
+ComparePlot <- ggplot(COUNTS, aes(x=V1, y=V3, colour=V2)) + theme_classic() + geom_bar() + scale_colour_brewer(palette="Set2")
+
+pdf(file=opt$output, width=8, height=6)
+	grid.table(Summary, show.rownames=FALSE)
+	ComparePlot
+dev.off()
