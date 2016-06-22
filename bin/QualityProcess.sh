b4a3df16980f338d736b49e2cd2a613414d1baa4
Geoffrey Hannigan <ghanni@upenn.edu>
2016-06-22 11:04:15 -0400

    Fixed sample ID call.

--- bin/QualityProcess.sh
+++ bin/QualityProcess.sh
@@ -52,10 +52,13 @@ runCutadaptWithMap () {
 	echo Input fastq = "${1}" #1 = full path to fastq file to be trimmed
 	echo Mapping file = "${2}" #2 = full path to mapping file mapping file
 	echo Output file = "${3}" #3 = full path for output directory
-	export SAMPLEID=$(echo ${1} | sed 's/_L001.*//g')
-	export THREEPRIME=$(awk --assign sampleid="$SAMPLEID" '$2 == sampleid { print $19 }' "${2}")
-	export FIVEPRIME=$(awk --assign sampleid=$SAMPLEID '$2 == sampleid { print $16 }' "${2}")
-	python2.7 "${CutAdapt}" --error-rate=0.1 --overlap=10 -a "$THREEPRIME" -a "$FIVEPRIME" "${1}" > "${3}"
+	export SAMPLEID=$(echo ${1} | sed 's/_S.._L001.*//g')
+	export THREEPRIME=$(awk --assign sampleid="$SAMPLEID" '$2 == sampleid { print $19 }' ${2})
+	export FIVEPRIME=$(awk --assign sampleid=$SAMPLEID '$2 == sampleid { print $16 }' ${2})
+	echo Sample ID is ${SAMPLEID}...
+	echo 3-prime adapter is ${THREEPRIME}...
+	echo 5-prime adapter is ${FIVEPRIME}...
+	python2.7 "${CutAdapt}" --error-rate=0.1 --overlap=10 -a $THREEPRIME -a $FIVEPRIME ${1} > ${3}
 }
 
 runFastx () {
