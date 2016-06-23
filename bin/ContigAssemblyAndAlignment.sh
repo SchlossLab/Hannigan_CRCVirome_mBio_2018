2a0691061867cbff7bb478e508e306aecb9dae20
Geoffrey Hannigan <ghanni@upenn.edu>
2016-06-23 13:27:57 -0400

    Fixed env setup.

--- bin/ContigAssemblyAndAlignment.sh
+++ bin/ContigAssemblyAndAlignment.sh
@@ -3,11 +3,19 @@
 # Pat Schloss Lab
 # University of Michigan
 
+#PBS -N ContigAssemblyAndAlignment
+#PBS -q first
+#PBS -l nodes=1:ppn=1,mem=40gb
+#PBS -l walltime=500:00:00
+#PBS -j oe
+#PBS -V
+#PBS -A schloss_lab
+
 # Use this shell script to subsample and prepare C diff
 # infection dataset for analysis on PMACS.
 
 # Set the variables to be used in this script
-export WorkingDirectory=/home/ghannig/projects/PrelimDataModi
+export WorkingDirectory=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ColonCancerVirome/data
 export Output='ContigsAndAlignments'
 
 export FileSource=/mnt/EXT/Schloss-data/ghannig/Hannigan-2016-ColonCancerVirome/data/QualitySeqs/DeconSeq
