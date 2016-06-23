2f88ac14604c930ab010b7e173df504fa9f7adc9
Geoffrey Hannigan <ghanni@upenn.edu>
2016-06-23 06:48:51 -0400

    Cleaned up the script a little.

--- bin/RunReadCountStats.R
+++ bin/RunReadCountStats.R
@@ -16,21 +16,21 @@ library("gridExtra")
 # Parse Input from Command Line #
 #################################
 
-option_list = list(
-	make_option(c("-c", "--counts"), type="character", default=NULL, 
-			help="Table with read count information", metavar="character"),
-	make_option(c("-o", "--output"), type="character", default=NULL, 
-			help="Output file for count summary", metavar="character"),
-	make_option(c("-p", "--png"), type="character", default=NULL, 
-			help="PNG output file for count summary", metavar="character"),
-	make_option(c("-t", "--title"), type="character", default=NULL,
-                        help="Title for the resulting plot", metavar="character"),
-	make_option(c("-l", "--log"), action="store_true", default=FALSE,
-			help="Should we use a log scale? [default %default]"),
-	make_option(c("-r", "--remove"), action="store_true", default=FALSE,
-			help="Remove negative controls from view [default %default]"),
-	make_option(c("-y", "--ylabel"), type="character", default=NULL,
-                        help="Label for y axis", metavar="character")
+option_list <- list(
+  make_option(c("-c", "--counts"), type = "character", default = NULL,
+      help = "Table with read count information", metavar = "character"),
+  make_option(c("-o", "--output"), type = "character", default = NULL,
+      help = "Output file for count summary", metavar = "character"),
+  make_option(c("-p", "--png"), type = "character", default = NULL,
+      help = "PNG output file for count summary", metavar = "character"),
+  make_option(c("-t", "--title"), type = "character", default = NULL,
+      help = "Title for the resulting plot", metavar = "character"),
+  make_option(c("-l", "--log"), action = "store_true", default = FALSE,
+      help = "Should we use a log scale? [default %default]"),
+  make_option(c("-r", "--remove"), action = "store_true", default = FALSE,
+      help = "Remove negative controls from view [default %default]"),
+  make_option(c("-y", "--ylabel"), type = "character", default = NULL,
+      help = "Label for y axis", metavar = "character")
 ); 
 
 opt_parser = OptionParser(option_list=option_list);
@@ -43,8 +43,8 @@ opt = parse_args(opt_parser);
 COUNTS <- read.delim(file=opt$counts, sep="\t", header=F)
 
 if (opt$remove) {
-	COUNTS <- COUNTS[c(!grepl("MG100008", COUNTS$V1)),]
-	COUNTS <- COUNTS[c(!grepl("MG100013", COUNTS$V1)),]
+  COUNTS <- COUNTS[c(!grepl("MG100008", COUNTS$V1)),]
+  COUNTS <- COUNTS[c(!grepl("MG100013", COUNTS$V1)),]
 }
 
 ############
@@ -54,23 +54,23 @@ if (opt$remove) {
 Summary <- summary(COUNTS)
 
 ComparePlot <- ggplot(COUNTS, aes(x=V1, y=V3, fill=V2)) + 
-	theme_classic() + 
-	geom_bar(stat = "identity", position="dodge") + 
-	scale_fill_brewer(palette="Set2") + 
-	theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
-	ylab(opt$title) +
-	xlab("Samples") +
-	coord_flip() +
-	ggtitle(opt$title)
+  theme_classic() + 
+  geom_bar(stat = "identity", position="dodge") + 
+  scale_fill_brewer(palette="Set2") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+  ylab(opt$title) +
+  xlab("Samples") +
+  coord_flip() +
+  ggtitle(opt$title)
 
 if (opt$log) {
    ComparePlot <- ComparePlot + scale_y_log10()
 }
 
 pdf(file=opt$output, width=8, height=6)
-	ComparePlot
+  ComparePlot
 dev.off()
 
 png(file=opt$png, width=8, height=6, units="in", res=300)
-	ComparePlot
+  ComparePlot
 dev.off()
