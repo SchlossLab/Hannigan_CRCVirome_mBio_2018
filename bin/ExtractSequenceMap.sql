62cefb620c5bda73b83ddde3683bd10b27a6f789
Geoffrey Hannigan <ghanni@upenn.edu>
2016-10-21 14:29:00 -0400

    Updated mapping file extraction script.

--- bin/ExtractSequenceMap.sql
+++ bin/ExtractSequenceMap.sql
@@ -3,8 +3,9 @@ use SchlossHanniganSequencing;
 
 /* Get the adapter information */;
 SELECT * 
-FROM SampleTracking, IndexPrimer001, IndexPrimer002 
-WHERE SampleTracking.Index1=IndexPrimer001.Index1 
-      AND SampleTracking.Index2=IndexPrimer002.Index2 
-      AND SampleTracking.SchlossID="NexteraXT002" 
-INTO OUTFILE '/Users/Hannigan/git/Hannigan-2016-ColonCancerVirome/data/NexteraXT002Map.tsv';
+FROM SampleTracking, IndexPrimer001, IndexPrimer002, PureViromeTracking 
+WHERE SampleTracking.Index1=IndexPrimer001.Index1
+      AND SampleTracking.Index2=IndexPrimer002.Index2
+      AND SampleTracking.SchlossID="NexteraXT003"
+      AND SampleTracking.UniqueSampleID=PureViromeTracking.UniqueSampleID
+INTO OUTFILE '/Users/Hannigan/git/Hannigan-2016-ColonCancerVirome/data/NexteraXT002Map2.tsv';
