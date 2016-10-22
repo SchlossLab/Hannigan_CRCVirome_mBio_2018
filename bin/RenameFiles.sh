91483740286ebad698aa25de697f4d855a9bf53c
Geoffrey Hannigan <ghanni@upenn.edu>
2016-10-22 15:11:06 -0400

    Test out the first sed

--- bin/RenameFiles.sh
+++ bin/RenameFiles.sh
@@ -5,14 +5,14 @@
 # University of Michigan
 
 for file in $(ls ./data/raw/NexteraXT003/*); do
-	filename=$(echo ${file} | sed 's/*\///g' | sed 's/_*//')
+	filename=$(echo ${file} | sed 's/*\///g')
 	echo File is ${file}
 	echo File name is ${filename}
-	# Get the alternate name to use for replacing
-	altname=$(awk -v name=${file} 'name = $13 {print $2)')
-	echo Alternate file name is ${altname}
-	# Now actually replace the file name
-	newname=$(echo ${file} | sed "s/$filename/$altname/" | sed 's/_[ATGC].*R/R/' | sed 's/_001//')
-	echo New file is ${newname}
+	# # Get the alternate name to use for replacing
+	# altname=$(awk -v name=${file} 'name = $13 {print $2)')
+	# echo Alternate file name is ${altname}
+	# # Now actually replace the file name
+	# newname=$(echo ${file} | sed "s/$filename/$altname/" | sed 's/_[ATGC].*R/R/' | sed 's/_001//')
+	# echo New file is ${newname}
 	# mv ${file} ${newname}
 done
