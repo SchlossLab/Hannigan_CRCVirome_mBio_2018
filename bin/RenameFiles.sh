2fbaa1f7678feb9958f3076e84ac886cedc6cb05
Geoffrey Hannigan <ghanni@upenn.edu>
2016-10-22 15:23:09 -0400

    Forgot to add a file.

--- bin/RenameFiles.sh
+++ bin/RenameFiles.sh
@@ -4,12 +4,14 @@
 # Pat Schloss Lab
 # University of Michigan
 
+export metadata=$1
+
 for file in $(ls ./data/raw/NexteraXT003/*); do
 	filename=$(echo ${file} | sed 's/.*\///g' | sed 's/_.*//')
 	echo File is ${file}
 	echo File name is ${filename}
 	# Get the alternate name to use for replacing
-	altname=$(awk -v name=${file} '$13 == name {print $2}')
+	altname=$(awk -v name=${file} '$13 == name {print $2}' ${metadata})
 	echo Alternate file name is ${altname}
 	# Now actually replace the file name
 	newname=$(echo ${file} | sed "s/$filename/$altname/" | sed 's/_[ATGC].*R/R/' | sed 's/_001//')
