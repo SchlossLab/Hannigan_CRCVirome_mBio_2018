db34a75e53d69111e9dcd619ff8a83b620367632
Geoffrey Hannigan <ghanni@upenn.edu>
2016-10-28 16:01:44 -0400

    Cluster the OPFs with e value less than 10e-10

--- bin/ClusterOPFs.sh
+++ bin/ClusterOPFs.sh
@@ -50,7 +50,7 @@ EstablishOpfs () {
 	mmseqs createdb ${1} DB
 
 	mkdir ./tmp
-    mmseqs cluster --min-seq-id 0.7 DB clu tmp
+    mmseqs cluster -e 0.0000000001 --min-seq-id 0.7 DB clu tmp
 
     # Convert to fasta
     mmseqs createseqfiledb DB clu clu_seq 
