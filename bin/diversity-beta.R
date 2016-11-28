4b3bb31a93782704c63184c445b099ccf255440c
Geoffrey Hannigan <ghanni@upenn.edu>
2016-11-28 12:18:31 -0500

    Added neg vs noneg outputs.

--- bin/diversity-beta.R
+++ bin/diversity-beta.R
@@ -22,7 +22,7 @@ option_list <- list(
   make_option(c("-i", "--input"),
     type = "character",
     default = NULL,
-    help = "OTU abundance table straight from Mothur.",
+    help = "OTU abundance table.",
     metavar = "character"),
   make_option(c("-m", "--metadata"),
     type = "character",
@@ -43,6 +43,11 @@ option_list <- list(
     type = "character",
     default = NULL,
     help = "Output pdf to draw results.",
+    metavar = "character"),
+  make_option(c("-n", "--negout"),
+    type = "character",
+    default = NULL,
+    help = "Output pdf to draw results with neg controls (QC).",
     metavar = "character")
 )
 
@@ -52,7 +57,9 @@ opt <- parse_args(opt_parser);
 ################
 # Run Analysis #
 ################
+# Import otu abundance table
 input <- read.delim(opt$input, header=TRUE, sep="\t")
+# Get the metadata table
 datadisease <- read.delim(opt$metadata, header=FALSE, sep="\t")[,c(2,30)]
 
 # Format for vegan
@@ -66,6 +73,10 @@ castsum <- rowSums(inputcast)
 inputcast <- rrarefy(inputcast, sample=opt$subsample)
 inputcast <- inputcast[c(rowSums(inputcast) %in% opt$subsample),]
 
+# Make one without negative controls as well
+negcastvector <- as.character(datadisease[c(datadisease$V30 %in% "Negative"),1])
+negcast <- inputcast[!c(rownames(inputcast) %in% negcastvector),]
+
 # Make the distance matrix
 castdist <- as.data.frame(as.matrix(vegdist(inputcast, method=opt$divmetric)))
 ORD_NMDS <- metaMDS(castdist,k=2)
@@ -75,6 +86,19 @@ NMDS_AND_MAP <- merge(ORD_FIT, datadisease, by.x="SampleID", by.y="V2")
 
 # Plot everything
 plotnmds <- ggplot(NMDS_AND_MAP, aes(x=MDS1, y=MDS2, colour=V30)) +
+    theme_classic() +
+    geom_point() +
+    scale_fill_manual(values = wes_palette("Royal1"))
+
+# Make the distance matrix without negative control data
+castdistnoneg <- as.data.frame(as.matrix(vegdist(negcast, method=opt$divmetric)))
+ORD_NMDS <- metaMDS(castdistnoneg,k=2)
+ORD_FIT = data.frame(MDS1 = ORD_NMDS$points[,1], MDS2 = ORD_NMDS$points[,2])
+ORD_FIT$SampleID <- rownames(ORD_FIT)
+NMDS_AND_MAP <- merge(ORD_FIT, datadisease, by.x="SampleID", by.y="V2")
+
+# Plot everything
+nonegplotnmds <- ggplot(NMDS_AND_MAP, aes(x=MDS1, y=MDS2, colour=V30)) +
     theme_classic() +
     geom_point() +
     scale_fill_manual(values = wes_palette("Royal1"))
@@ -100,8 +124,34 @@ plotdiffs <- ggplot(moddf, aes(y=diff, x=comparison)) +
     ylab("Differences in Mean Levels of Group") +
     xlab("")
 
+# Dispersion without negatives as well
+catdistnames <- castdistnoneg
+catdistnames$names <- row.names(catdistnames)
+mergedist <- merge(catdistnames, datadisease, by.x="names", by.y="V2")
+dist <- vegdist(inputcast, method=opt$divmetric)
+mod <- betadisper(dist, mergedist[,length(mergedist)])
+anova(mod)
+permutest(mod, pairwise = TRUE)
+mod.HSD <- TukeyHSD(mod)
+
+moddf <- as.data.frame(mod.HSD$group)
+moddf$comparison <- row.names(moddf)
+limits <- aes(ymax = upr, ymin=lwr)
+nonegplotdiffs <- ggplot(moddf, aes(y=diff, x=comparison)) +
+    theme_classic() +
+    geom_pointrange(limits) +
+    geom_hline(yintercept=0, linetype = "dashed") +
+    coord_flip() +
+    ylab("Differences in Mean Levels of Group") +
+    xlab("")
+
 gridplot <- plot_grid(plotnmds, plotdiffs, labels = c("A", "B"))
+gridplotnoneg <- plot_grid(plotnmds, plotdiffs, labels = c("A", "B"))
 
 pdf(file=opt$out, width=10, height=4)
+    gridplotnoneg
+dev.off()
+
+pdf(file=opt$negout, width=10, height=4)
     gridplot
 dev.off()
