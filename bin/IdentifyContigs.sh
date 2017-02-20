0ddbdf0fa061e5e5d62ff5998e9c7f10ca745cca
Geoffrey Hannigan <ghanni@upenn.edu>
2017-02-20 11:09:38 -0500

    Updated to run blast on bacteria

--- bin/IdentifyContigs.sh
+++ bin/IdentifyContigs.sh
@@ -36,54 +36,54 @@ RunBlast () {
 
 export -f RunBlast
 
-# cut -f 1 ${idfile} | tail -n +2 > ./data/tmpid/contiglist.tsv
-# grep -A 1 -f ./data/tmpid/contiglist.tsv ${fastafile} \
-# 	| egrep -v "\-\-" \
-# 	> ./data/tmpid/contigrepset.fa
+cut -f 1 ${idfile} | tail -n +2 > ./data/tmpid/contiglist.tsv
+grep -A 1 -f ./data/tmpid/contiglist.tsv ${fastafile} \
+	| egrep -v "\-\-" \
+	> ./data/tmpid/contigrepset.fa
 
-# RunBlast ./data/tmpid/contigrepset.fa ${referencefile}
+RunBlast ./data/tmpid/contigrepset.fa ${referencefile}
 
-# # Add cluster ID to the table
-# cut -f 1,2 ./data/tmpid/blastout.tsv | sort | uniq > ./data/tmpid/cutblastout.tsv
-# sed 's/\,/\t/' ./data/ContigClustersVirus/clustering_gt1000.csv > ./data/tmpid/clusterids.tsv
-# awk -F "\t" 'FNR==NR { a[$1] = $2; next } { for( i in a ) if($1 ~ i) {print a[$1]"\t"$2} }' \
-# 	./data/tmpid/clusterids.tsv \
-# 	./data/tmpid/cutblastout.tsv \
-# 	| sed 's/ENA|\(.\+\)|.*/\1/' \
-# 	> ./data/tmpid/clustform.tsv
+# Add cluster ID to the table
+cut -f 1,2 ./data/tmpid/blastout.tsv | sort | uniq > ./data/tmpid/cutblastout.tsv
+sed 's/\,/\t/' ./data/ContigClustersVirus/clustering_gt1000.csv > ./data/tmpid/clusterids.tsv
+awk -F "\t" 'FNR==NR { a[$1] = $2; next } { for( i in a ) if($1 ~ i) {print a[$1]"\t"$2} }' \
+	./data/tmpid/clusterids.tsv \
+	./data/tmpid/cutblastout.tsv \
+	| sed 's/ENA|\(.\+\)|.*/\1/' \
+	> ./data/tmpid/clustform.tsv
 
 # Make list of IDs
 cut -f 2 ./data/tmpid/clustform.tsv > ./data/tmpid/tmplist.tsv
 
 grep --file=./data/tmpid/tmplist.tsv ./tmp-database-download/nucl_gb.accession2taxid > ./data/tmpid/taxlist.tsv
 
-# Get the taxon IDs
-awk -F "\t" 'FNR==NR { a[$1] = $3; next } { for( i in a ) if($2 ~ i) {print $1"\t"$2"\t"a[$2]} }' \
-	./data/tmpid/taxlist.tsv  \
-	./data/tmpid/clustform.tsv \
-	> ./data/tmpid/ctax.tsv
-cut -f 3 ./data/tmpid/ctax.tsv > ./data/tmpid/tmplist.tsv
-# Download the information using the EBI API
-while read line; do
-	wget -O - "http://www.ebi.ac.uk/ena/data/taxonomy/v1/taxon/tax-id/${line}" \
-	| grep "lineage" \
-	| sed 's/.\+\: \"//' \
-	| sed 's/\; \".*//' \
-	| sed 's/\; /\t/g' \
-	| sed 's/ /_/g' \
-	| sed "s/^/${line}\t/"
-done < ./data/tmpid/tmplist.tsv > ./data/tmpid/taxawnum.tsv
+# # Get the taxon IDs
+# awk -F "\t" 'FNR==NR { a[$1] = $3; next } { for( i in a ) if($2 ~ i) {print $1"\t"$2"\t"a[$2]} }' \
+# 	./data/tmpid/taxlist.tsv  \
+# 	./data/tmpid/clustform.tsv \
+# 	> ./data/tmpid/ctax.tsv
+# cut -f 3 ./data/tmpid/ctax.tsv > ./data/tmpid/tmplist.tsv
+# # Download the information using the EBI API
+# while read line; do
+# 	wget -O - "http://www.ebi.ac.uk/ena/data/taxonomy/v1/taxon/tax-id/${line}" \
+# 	| grep "lineage" \
+# 	| sed 's/.\+\: \"//' \
+# 	| sed 's/\; \".*//' \
+# 	| sed 's/\; /\t/g' \
+# 	| sed 's/ /_/g' \
+# 	| sed "s/^/${line}\t/"
+# done < ./data/tmpid/tmplist.tsv > ./data/tmpid/taxawnum.tsv
 
-rm -rf ./data/tmpcter
-mkdir -p ./data/tmpcter
-for i in $(seq 1 8); do
-	awk -v col=$i '{ if($col) print $col; else print "Unclassified" }' ./data/tmpid/taxawnum.tsv > ./data/tmpcter/${i}.tsv
-done
-paste ./data/tmpcter/* > ./data/tmpid/fullcols.tsv
+# rm -rf ./data/tmpcter
+# mkdir -p ./data/tmpcter
+# for i in $(seq 1 8); do
+# 	awk -v col=$i '{ if($col) print $col; else print "Unclassified" }' ./data/tmpid/taxawnum.tsv > ./data/tmpcter/${i}.tsv
+# done
+# paste ./data/tmpcter/* > ./data/tmpid/fullcols.tsv
 
-awk -F "\t" 'FNR==NR { a[$3] = $1"\t"$2; next } { for( i in a ) if($1 ~ i) {print a[$1]"\t"$0} }' \
-	./data/tmpid/ctax.tsv  \
-	./data/tmpid/fullcols.tsv \
-	> ./data/contigclustersidentity/clustax.tsv
+# awk -F "\t" 'FNR==NR { a[$3] = $1"\t"$2; next } { for( i in a ) if($1 ~ i) {print a[$1]"\t"$0} }' \
+# 	./data/tmpid/ctax.tsv  \
+# 	./data/tmpid/fullcols.tsv \
+# 	> ./data/contigclustersidentity/clustax.tsv
 
 
