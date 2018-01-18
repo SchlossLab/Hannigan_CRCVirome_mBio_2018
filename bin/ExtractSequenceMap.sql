1d3627c52b70caa726abf69b12df9f4af2589e31
Geoffrey Hannigan <ghanni@upenn.edu>
2018-01-17 21:59:24 -0500

    Updated SQL extraction file for metadata.

--- bin/ExtractSequenceMap.sql
+++ bin/ExtractSequenceMap.sql
@@ -6,6 +6,14 @@ SELECT *
 FROM SampleTracking, IndexPrimer001, IndexPrimer002, PureViromeTracking 
 WHERE SampleTracking.Index1=IndexPrimer001.Index1
       AND SampleTracking.Index2=IndexPrimer002.Index2
+      AND SampleTracking.SchlossID="NexteraXT003"
+      AND SampleTracking.UniqueSampleID=PureViromeTracking.UniqueSampleID
+INTO OUTFILE '/Users/Hannigan/git/Hannigan-2016-ColonCancerVirome/data/metadata/NexteraXT003Map.tsv';
+
+SELECT * 
+FROM SampleTracking, IndexPrimer001, IndexPrimer002, PureViromeTracking 
+WHERE SampleTracking.Index1=IndexPrimer001.Index1
+      AND SampleTracking.Index2=IndexPrimer002.Index2
       AND SampleTracking.SchlossID="NexteraXT004"
       AND SampleTracking.UniqueSampleID=PureViromeTracking.UniqueSampleID
 INTO OUTFILE '/Users/Hannigan/git/Hannigan-2016-ColonCancerVirome/data/metadata/NexteraXT004Map.tsv';
